e1a7fc65adee59794f5f7e66588c515054b78b23
kashish00208 <kgupta7827@gmail.com>
2024-08-28 23:09:31 +0530

    pages

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -5,10 +5,10 @@ function App() {
   return (
     <>
       <main className="bg-custom-gradient  w-screen h-screen">
-        <div className="w-4/5">
-          <div className="bg-custom-dark">
-            <div className="section-1">
-              {/* <img src={Img} className="rounded-3xl" alt="Image" /> */}
+        <div className="w-3/5 pl-40 pt-12">
+          <div className="bg-custom-dark py-8 rounded-3xl">
+            <div className="section-1 w-full h-full pl-5 pr-5">
+              <img src={Img} className="rounded-3xl" alt="Image" />
             </div>
             <div className="section-2">
               <h1>WeatherWave</h1>
