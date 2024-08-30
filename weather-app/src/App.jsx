00c12de754224a2ab851c30fad0b9bbb526d0df9
kashish00208 <kgupta7827@gmail.com>
2024-08-30 11:14:01 +0530

    updated

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -22,7 +22,6 @@ function App() {
             <h2 className="text-2xl md:text-3xl font-bold text-white mb-2">WeatherWave</h2>
             <p className="text-sm md:text-base text-white mb-4">A weather App</p>
             <button onClick={handleclick} className="w-full md:w-auto text-sm bg-blue-400 text-white rounded-3xl px-4 py-2 ">Get Started</button>
-            
           </div>
         </div>
       </div>
