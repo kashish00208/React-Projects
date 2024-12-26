b6491965abd7340eb0d66f1e6475721481c17d64
kashish00208 <kgupta7827@gmail.com>
2024-12-26 13:52:35 +0530

    Edit

--- CodeWave/src/App.jsx
+++ CodeWave/src/App.jsx
@@ -1,5 +1,16 @@
+import React from "react";
+import Editor from "./Components/Editor";
 function App() {
-  return <></>;
+  return <>
+  <div className="Editor-set">
+    <Editor/>
+    <Editor/>
+    <Editor/>
+  </div>
+  <div className="output-set">
+    <iframe title="output" sandbox="allow-scripts" width={100} height={100} frameborder="0"/>
+  </div>
+  </>;
 }
 
 export default App;
